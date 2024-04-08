0e7f900181675ff1c3fadd8f1558c9cf83fb5197
Vespertine <hillbgh@gmail.com>
2024-04-08 15:01:45 -0600

    Updated my keymap / firmware

--- keyboards/crkbd/rev1/keymaps/Vespertine112/rules.mk
+++ keyboards/crkbd/rev1/keymaps/Vespertine112/rules.mk
@@ -4,7 +4,6 @@
 MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 WPM_ENABLE 		= yes
 
